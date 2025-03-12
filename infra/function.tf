b7fd68a8e9841240b487e4eac19d0547ba33e00d
Viktor-Soltesz <solteesz.viktor@gmail.com>
2025-03-12 22:46:26 +0100

    modularized python part within cloud function

--- infra/function.tf
+++ infra/function.tf
@@ -19,7 +19,7 @@ resource "google_cloudfunctions2_function" "function" {
   
   build_config {
     runtime     = "python310"
-    entry_point = "trigger_gcs" # Set the entry point in the code
+    entry_point = "gcs_to_bq" # Set the entry point in the code
 
     source {
       storage_source {
