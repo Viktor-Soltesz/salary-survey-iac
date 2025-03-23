cc8a6bf1f4f1fda06af8e716d854275351655f4a
Viktor-Soltesz <solteesz.viktor@gmail.com>
2025-03-23 19:14:12 +0100

    Corrected: json files were not inside the etl_pipeline folder, yaml zipping was not including them

--- etl_pipeline/transform/map_country_codes.py
+++ etl_pipeline/transform/map_country_codes.py
@@ -15,7 +15,7 @@ def map_country_codes(df: pd.DataFrame) -> pd.DataFrame:
 
     # Load country mapping from JSON file
     #mapping_path = os.path.join(os.path.dirname(__file__), 'country_mapping.json')
-    mapping_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'assets', 'country_mapping.json')
+    mapping_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.', 'assets_etl', 'country_mapping.json')
     mapping_path = os.path.abspath(mapping_path)
     
     with open(mapping_path, 'r', encoding='utf-8') as file:
--- etl_pipeline/transform/map_job_categories.py
+++ etl_pipeline/transform/map_job_categories.py
@@ -21,7 +21,7 @@ def map_job_categories(df: pd.DataFrame) -> pd.DataFrame:
 
     # Load job category mapping from JSON file
     #mapping_path = os.path.join(os.path.dirname(__file__), 'job_categories.json')
-    mapping_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'assets', 'job_categories.json')
+    mapping_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.', 'assets_etl', 'job_categories.json')
     mapping_path = os.path.abspath(mapping_path)
 
     with open(mapping_path, 'r', encoding='utf-8') as file:
